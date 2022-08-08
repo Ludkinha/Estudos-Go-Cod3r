c00cc86bdf9e39a2033386797de1fb626ccc6d6d
Lucas Bergamin <lucasgithub43@gmail.com>
2022-08-08 18:05:50 -0400

    Update tipos.go

--- Fundamentos/tipos/tipos.go
+++ Fundamentos/tipos/tipos.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+)
+
+func main() {
+	//números inteiros
+	fmt.Println(1, 2, 1000)
+	fmt.Println("Literal inteiro é", reflect.TypeOf(3200))
+
+	//sem sinal (só positivos)... uint8 uint16 uint32 uint64 (Unsigned Int)
+	var b byte = 255
+	fmt.Println("O byte é", reflect.TypeOf(b))
+
+	//com sinal... int8 int16 int32 int64
+	i1 := math.MaxInt64
+	fmt.Println("O valor máximo do int é", i1)
+}
