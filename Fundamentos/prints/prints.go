d89a4b58a259c62a5088932e6a00f4ff9b99a05d
Lucas Bergamin <lucasgithub43@gmail.com>
2022-08-08 00:02:56 -0400

    Printf e algumas funções!!!

--- Fundamentos/prints/prints.go
+++ Fundamentos/prints/prints.go
@@ -6,7 +6,20 @@ func main() {
 	fmt.Println("Mesma")
 	fmt.Print("Linha.")
 
-	x := 777
+	x := 7.171313
 
 	fmt.Println("O valor de x é ", x)
+
+	fmt.Printf("Significa Print Formatado, o valor de x é %.2f", x)
+
+	a := 1
+	b := 1.9999
+	c := false
+	d := "opa"
+
+	//Nota-se que alguns número são arredondados automaticamente pelo sistema!
+	fmt.Printf("\n%d %f %.2f %t %s", a, b, b, c, d)
+	fmt.Printf("\n%v %v %v %v", a, b, c, d)
+
+
 }
